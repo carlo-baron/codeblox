7cd03fc47143cb88486dd51cd2f6a517c5e52d1b
carlo-baron <carlobaron430@gmail.com>
2025-11-14 20:28:03 +0800

    feat: added bounds and collision detection
    refactor: renamed movement to mover

--- Assets/scripts/movement/Mover.cs
+++ Assets/scripts/movement/Mover.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class Movement : MonoBehaviour, IMover
+public class Mover : MonoBehaviour, IMover
 {
     public Queue<Vector2> Moves { get; private set; }
     public bool CanMove { get; set; } = true;
@@ -12,11 +12,23 @@ public class Movement : MonoBehaviour, IMover
     }
 
     public void Move(float x, float y){
+        x = Mathf.Clamp(x, -1, 1);
+        y = Mathf.Clamp(y, -1, 1);
+
+        if(Obstructed(x, y)) return;
+
         Vector2 newPos = new Vector2(
                 transform.position.x + (x * CELL_SIZE),
                 transform.position.y + (y * CELL_SIZE)
                 );
         Moves.Enqueue(newPos);
         transform.position = newPos;
+    }
+
+    private bool Obstructed(float x, float y){
+        Vector2 dir = new Vector2(x, y);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1);
+        
+        return hit;
     }
 }
