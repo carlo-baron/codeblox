b685800bf01d12a22f1e802a4d0552bc024e6ad8
carlo-baron <carlobaron430@gmail.com>
2025-11-16 12:42:21 +0800

    feat: move other objects temp behaviour

--- Assets/scripts/movement/Mover.cs
+++ Assets/scripts/movement/Mover.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Mover : MonoBehaviour, IMover
 {
@@ -7,28 +8,57 @@ public class Mover : MonoBehaviour, IMover
     public bool CanMove { get; set; } = true;
     int CELL_SIZE = 1;
 
-    public void Start(){
+    public void Start()
+    {
         Moves = new Queue<Vector2>();
     }
 
-    public void Move(float x, float y){
+    public void Move(float x, float y)
+    {
         x = Mathf.Clamp(x, -1, 1);
         y = Mathf.Clamp(y, -1, 1);
 
-        if(Obstructed(x, y)) return;
+        Vector2 dir = new Vector2(x, y);
+
+        RaycastHit2D hit = Obstruction(dir);
+
+        if (hit.collider && hit.collider.gameObject != gameObject)
+        {
+            TryMoveObstruction(hit.collider.gameObject, dir);
+            return;
+        }
 
         Vector2 newPos = new Vector2(
-                transform.position.x + (x * CELL_SIZE),
-                transform.position.y + (y * CELL_SIZE)
-                );
+            transform.position.x + (x * CELL_SIZE),
+            transform.position.y + (y * CELL_SIZE)
+        );
+
         Moves.Enqueue(newPos);
         transform.position = newPos;
     }
 
-    private bool Obstructed(float x, float y){
-        Vector2 dir = new Vector2(x, y);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1);
-        
-        return hit;
+    private void TryMoveObstruction(GameObject hit, Vector2 dir)
+    {
+        IMover movable = hit.GetComponentInChildren<IMover>();
+        if (movable != null){
+            movable.Move(dir.x, dir.y);
+        }
+    }
+
+    private RaycastHit2D Obstruction(Vector2 dir)
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, dir, 1f);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider != null && hit.collider.transform != transform)
+            {
+                print(transform.parent.name + ": " + hit.collider.transform.name);
+                return hit;
+            }
+        }
+
+        return new RaycastHit2D();
     }
 }
+
